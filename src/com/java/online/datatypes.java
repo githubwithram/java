59eced0467a2d4bf23c40329c2e36a285d9aa3a4
rams git practice <shoppings.reddy@gmail.com>
2023-09-06 23:37:14 +0530

    data types

---
+++ src/com/java/online/datatypes.java
@@ -0,0 +1,25 @@
+package com.java.online;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		int firstvalue = 500;
+		int b = 20;
+		int c = firstvalue*b;
+		short pen = 55;
+		char value = 'R';
+		char chr = ';';
+		boolean Value=true;
+		double salary = 999123.456;
+		String city = "bengaluru";
+		String empid = "903193088";
+		System.out.println(city+pen+salary+chr+value);		
+		System.out.println(empid);
+		System.out.println(firstvalue);
+		System.out.println(pen);
+		System.out.println(Value);
+		System.out.println(chr);
+		System.out.println(salary);
+	}
+
+}
